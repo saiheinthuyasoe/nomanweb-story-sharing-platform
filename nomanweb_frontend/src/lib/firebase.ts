5da04860e742a003495a4a10180ce294913b618a
Sai Hein Thu Ya Soe <135956428+saiheinthuyasoe@users.noreply.github.com>
2025-06-18 06:36:47 +0700

    remove google keys

--- nomanweb_frontend/src/lib/firebase.ts
+++ nomanweb_frontend/src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY||"AIzaSyCZmt2rv6mRCa5as6ja39lbN7OR5m5O2E8",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN||"nomanweb-ssp.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID||"nomanweb-ssp",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET||"nomanweb-ssp.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID||"178736588876",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID||"1:178736588876:web:7d707f671e61cca5526ce9",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID||"G-GRSV3JGZZP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
