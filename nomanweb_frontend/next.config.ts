749fb502189cc9020280df56581162cf6e917b5f
Sai Hein Thu Ya Soe <135956428+saiheinthuyasoe@users.noreply.github.com>
2025-09-22 18:25:35 +0700

    Next config fixed

--- nomanweb_frontend/next.config.ts
+++ nomanweb_frontend/next.config.ts
@@ -47,7 +47,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api"}/:path*`,
       },
     ];
   },
